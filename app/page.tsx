4a7559ed328a5e4c51c7f8c86f9a3906ab6a7fd4
Flosrn <flosrn.dev@gmail.com>
2023-04-04 16:28:35 +0200

    feat(mobile): add mobile menu

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,13 @@ export default async function IndexPage() {
         <HeroAnimateIcons />
         <div className="flex max-w-[980px] flex-col items-center gap-2">
           <h1 className="text-center text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-            Homaide.art
+            myposter.ai
           </h1>
           <h2 className="text-center text-2xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-4xl">
             Generate beautiful posters in seconds with AI
           </h2>
           <p className="max-w-[700px] text-lg text-slate-700 dark:text-slate-400 sm:text-xl">
-            Homaide.art is a free tool that uses AI to generate your own
+            myposter.ai is a free tool that uses AI to generate your own
             beautiful posters in seconds. Create it and print it, it's so easy,
             you will receive your poster in 3 days.
           </p>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React from "react";
+import { Menu } from "lucide-react";
 
 import DropdownMenuNav from "~/components/DropdownMenuNav";
 import { Navbar } from "~/components/Navbar";
@@ -9,21 +10,34 @@ import { CartDrawer } from "~/components/shopping-cart/CartDrawer";
 type HeaderProps = {};
 
 const Header = ({}: HeaderProps) => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <header className="sticky top-0 z-40 w-full border-b border-b-slate-200 bg-white dark:border-b-slate-700 dark:bg-slate-900">
       <div className="container flex h-16 items-center">
-        <div className="hidden flex-1 md:flex md:items-center md:justify-between">
+        <div className="flex flex-1 items-center justify-between">
           <div className="flex">
+            <div className="md:hidden">
+              <button
+                onClick={handleMenuToggle}
+                className="rounded p-1 focus:outline-none focus:ring-2 focus:ring-blue-400"
+              >
+                <Menu size={24} />
+              </button>
+            </div>
             <a className="mr-6 flex items-center space-x-2" href="/">
               <span className="hidden font-bold sm:inline-block">
-                homaide.art
+                myposter.ai
               </span>
             </a>
             <nav
               aria-label="Main"
               data-orientation="horizontal"
               dir="ltr"
-              className="relative z-10 flex flex-1 items-center justify-center"
+              className="relative z-10 hidden flex-1 md:flex md:items-center md:justify-center"
             >
               <Navbar />
               <div className="absolute left-0 top-full flex justify-center" />
@@ -35,6 +49,13 @@ const Header = ({}: HeaderProps) => {
           </div>
         </div>
       </div>
+      {isMenuOpen && (
+        <div className="md:hidden">
+          <nav aria-label="Mobile Main" className="w-full">
+            <Navbar />
+          </nav>
+        </div>
+      )}
     </header>
   );
 };
