0e9760ddeb1243fea4a44ca5ae4b679bae9b2178
Flosrn <flosrn.dev@gmail.com>
2023-04-05 19:16:33 +0200

    fix(auth): revert

--- src/server/my-custom-adapter.ts
+++ src/server/my-custom-adapter.ts
@@ -3,23 +3,17 @@ import type { PrismaClient } from "@prisma/client";
 import type { Adapter, AdapterUser } from "next-auth/adapters";
 import { getUniqueUsername } from "~/utils/getUniqueUsername";
 
-type CustomAdapterUser = Omit<AdapterUser, "id"> & {
-  name?: string | null;
-  image?: string | null;
-};
-
 export const MyCustomAdapter = (prisma: PrismaClient): Adapter => ({
   ...PrismaAdapter(prisma),
-  async createUser(user: CustomAdapterUser) {
+  async createUser(user) {
     const username = await getUniqueUsername(prisma, user.email);
-    const userData: Record<string, unknown> = {
-      ...user,
-      username,
-      name: user.name ?? "",
-      image: user.image ?? "",
-    };
     const newUser = await prisma.user.create({
-      data: userData as never,
+      data: {
+        ...user,
+        username,
+        name: user.name ?? "",
+        image: user.image ?? "",
+      },
     });
     return newUser as AdapterUser;
   },
