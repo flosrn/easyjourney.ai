7ea1a11bf6793ccdea9ee3d9570450dd91bdd1b5
Flosrn <flosrn.dev@gmail.com>
2023-04-06 11:33:50 +0200

    feat(prisma): update schema

---
+++ prisma/migrations/20230406093314_dates/migration.sql
@@ -0,0 +1,40 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createdAt` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `accounts` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `sessions` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `createdAt` on the `verification_tokens` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `verification_tokens` table. All the data in the column will be lost.
+  - Added the required column `updated_at` to the `accounts` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `sessions` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `users` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updated_at` to the `verification_tokens` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "accounts" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "sessions" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "verification_tokens" DROP COLUMN "createdAt",
+DROP COLUMN "updatedAt",
+ADD COLUMN     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updated_at" TIMESTAMP(3) NOT NULL;
