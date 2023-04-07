e568e22812e1f72793a956791d3eea5323a0faf6
Flosrn <flosrn.dev@gmail.com>
2023-04-07 09:48:47 +0200

    feat(poster): remove layout

--- app/posters/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React, { type PropsWithChildren } from "react";
-
-const CreateLayout = async ({ children }: PropsWithChildren) => {
-  return <>{children}</>;
-};
-
-export default CreateLayout;
