e3a3ae86addbdb793c253e1b2b3872e5331e22d1
HugoSbl <hugo.sablot@gmail.com>
2023-04-09 18:58:57 +0200

    feat(pricing): added pricing Data and minor modifications to the pricing page

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,15 +1,17 @@
+
+const pricingData = [
+  {title: 'Free', price: '$0', description: 'For small teams or hobby projects', features: ['Up to 5 users', 'Basic support', 'All core features']},
+  {title: 'Pro', price: '$15', description: 'Most popular choice', features: ['Up to 20 users', 'Priority email support', 'All core features', 'Enhanced security']},
+  {title: 'Enterprise', price: '$30', description: 'For large teams or enterprises', features: ['Unlimited users', 'Phone & email support', 'All core features', 'Enhanced security', 'Custom integrations', `double line checked if this is very long then what will happen ? Will this bug ? Will this broke ? I don't know`]}
+];
+
 export default function PricingPage() {
   return (
     <>
-      <section
-        className="container mt-24 grid items-center gap-6 pt-6 pb-8 md:py-10">
-        <div className="flex max-w-[980px] flex-col items-start gap-2">
-          <h1
-            className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-            About
-          </h1>
-        </div>
-      </section>
+      <div className="flex-col flex-center mt-6">
+      <h2 className="uppercase font-light text-xl tracking-wider">Pricing</h2>
+      <span className="text-4xl font-extrabold mt-6">Choose the way you make Art</span>
+      </div>
     </>
   );
 }
