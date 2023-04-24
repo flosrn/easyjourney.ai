c3e48a96d044587d202e1a65327dc6188b112c88
Flosrn <flosrn.dev@gmail.com>
2023-04-24 15:44:43 +0200

    feat(ui-components): update tailwind.config.js and global styles

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,3 +1,5 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/Tabs";
+
 export default function AboutPage() {
   return (
     <>
@@ -6,6 +8,14 @@ export default function AboutPage() {
           <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
             About
           </h1>
+          <Tabs defaultValue="account" className="w-[400px]">
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="account">Account</TabsTrigger>
+              <TabsTrigger value="password">Password</TabsTrigger>
+            </TabsList>
+            <TabsContent value="account">account</TabsContent>
+            <TabsContent value="password">password</TabsContent>
+          </Tabs>
         </div>
       </section>
     </>
