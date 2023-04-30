2c555f1af594b2a3fa5e7bbdc426b2d9e5883a59
Flosrn <flosrn.dev@gmail.com>
2023-04-30 21:56:44 +0200

    feat(create-poster): refactoring

--- app/api/imagine/route.ts
+++ app/api/imagine/route.ts
@@ -1,5 +1,65 @@
 import { NextResponse } from "next/server";
-import { imagine } from "~/services/midjourneyUtils";
+import { channelId, headers, serverId } from "~/services/midjourneyUtils";
+
+const imaginePayload = {
+  type: 2,
+  application_id: "936929561302675456",
+  guild_id: serverId,
+  channel_id: channelId,
+  session_id: "2fb980f65e5c9a77c96ca01f2c242cf6",
+  data: {
+    version: "1077969938624553050",
+    id: "938956540159881230",
+    name: "imagine",
+    type: 1,
+    options: [
+      {
+        type: 3,
+        name: "prompt",
+      },
+    ],
+    application_command: {
+      id: "938956540159881230",
+      application_id: "936929561302675456",
+      version: "1077969938624553050",
+      default_permission: true,
+      default_member_permissions: null,
+      type: 1,
+      nsfw: false,
+      name: "imagine",
+      description: "Create images with Midjourney",
+      dm_permission: true,
+      options: [
+        {
+          type: 3,
+          name: "prompt",
+          description: "The prompt to imagine",
+          required: true,
+        },
+      ],
+    },
+    attachments: [],
+  },
+};
+
+export const imagine = async ({
+  prompt,
+}: {
+  prompt: string;
+}): Promise<number> => {
+  const response = await fetch("https://discord.com/api/v9/interactions", {
+    method: "POST",
+    headers: { ...headers, "Content-Type": "application/json" },
+    body: JSON.stringify({
+      ...imaginePayload,
+      data: {
+        ...imaginePayload.data,
+        options: [{ ...imaginePayload.data.options[0], value: prompt }],
+      },
+    }),
+  });
+  return response.status;
+};
 
 export async function POST(request: Request) {
   const { prompt } = await request.json();
--- app/api/variation/route.ts
+++ app/api/variation/route.ts
@@ -1,5 +1,35 @@
 import { NextResponse } from "next/server";
-import { variation } from "~/services/midjourneyUtils";
+import { channelId, headers, serverId } from "~/services/midjourneyUtils";
+
+export const variation = async ({
+  index,
+  messageId,
+  messageHash,
+}: {
+  index: number;
+  messageId: string;
+  messageHash: string;
+}): Promise<number> => {
+  const response = await fetch("https://discord.com/api/v9/interactions", {
+    method: "POST",
+    headers: { ...headers, "Content-Type": "application/json" },
+    body: JSON.stringify({
+      type: 3,
+      nonce: "1102305268038893568",
+      guild_id: serverId,
+      channel_id: channelId,
+      message_flags: 0,
+      message_id: messageId,
+      application_id: "936929561302675456",
+      session_id: "65f2874151c7356c87116067588df7e5",
+      data: {
+        component_type: 2,
+        custom_id: `MJ::JOB::variation::${index}::${messageHash}`,
+      },
+    }),
+  });
+  return response.status;
+};
 
 export async function POST(request: Request) {
   const body = await request.json();
