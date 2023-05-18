87fa2524644e01fedeaf4accfd2df36b9a445691
Flosrn <flosrn.dev@gmail.com>
2023-05-18 16:50:28 +0200

    feat(filters): improve badges

--- app/(dashboard)/create/components/badge/FiltersBadge.tsx
+++ app/(dashboard)/create/components/badge/FiltersBadge.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
 import { Badge } from "~/components/ui/Badge";
 
 import { useFilterStore } from "../../store/filterStore";
@@ -11,24 +13,27 @@ const FiltersBadge = () => {
     state.removeFilter,
   ]);
   return (
-    <div className="mt-6 flex h-[30px] justify-start space-x-1">
-      {selectedFilters.map((filter) => {
-        const isAlreadySelected = selectedFilters.some(
-          (selectedFilter) => selectedFilter.id === filter.id
-        );
-        return (
-          <Badge
-            key={filter.id}
-            onClick={() => {
-              isAlreadySelected ? removeFilter(filter) : addFilter(filter);
-            }}
-            variant="outline"
-            className="cursor-pointer"
-          >
-            {filter.name}
-          </Badge>
-        );
-      })}
+    <div className="mt-6 h-[30px]">
+      <Swiper slidesPerView="auto" spaceBetween={5}>
+        {selectedFilters.map((filter) => {
+          const isAlreadySelected = selectedFilters.some(
+            (selectedFilter) => selectedFilter.id === filter.id
+          );
+          return (
+            <SwiperSlide key={filter.id} className="!w-fit">
+              <Badge
+                onClick={() => {
+                  isAlreadySelected ? removeFilter(filter) : addFilter(filter);
+                }}
+                variant="outline"
+                className="shrink-0 cursor-pointer"
+              >
+                {filter.name}
+              </Badge>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 };
