7a789084df48b5c2d493ad40cd368e648f2d3c74
HugoSbl <hugo.sablot@gmail.com>
2023-05-18 14:25:05 +0200

    feat(favorite-page): poster calling

--- app/(marketing)/profile/[username]/likes/page.tsx
+++ app/(marketing)/profile/[username]/likes/page.tsx
@@ -1,13 +1,31 @@
+import { Suspense } from "react";
 import type { User } from "@prisma/client";
 import { getServerAuthSession } from "~/server/auth";
 import { prisma } from "~/server/db/prisma";
+
+import Posters from "../../../posters/Posters";
 
 type UserProfileProps = {
   params: { username: User["username"] };
 };
 
-const Likes = async (username) => {
-  return <div>TEST</div>;
-};
+const getUserLikedPosters = async (username) =>
+  prisma.like.findMany({
+    where: { id: username },
+    include: { poster: true },
+  });
 
-export default Likes;
+export default async function Likes({
+  params: { username },
+}: UserProfileProps) {
+  const posterLiked = await getUserLikedPosters(username);
+  console.log("posters", posterLiked);
+
+  return (
+    <div>
+      <Suspense fallback={<div>Loading posters...</div>}>
+        <Posters posters={posterLiked} />
+      </Suspense>
+    </div>
+  );
+}
