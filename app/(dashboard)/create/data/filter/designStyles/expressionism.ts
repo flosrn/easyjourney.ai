c006027e0f7a6aba8c4935733a9c5e8b5df74e33
MDegano <123469601+MDeganoWDev@users.noreply.github.com>
2023-05-18 11:59:16 +0200

    end second category

---
+++ app/(dashboard)/create/data/filter/designStyles/expressionism.ts
@@ -0,0 +1,38 @@
+import type { Filter } from "../typeFilters";
+
+export const expressionismFilters: Filter[] = [
+  {
+    id: "Expressionism_8_3_1",
+    name: "Expressionism",
+    description: "Bold and emotional artistic style",
+    style: "Expressionism",
+    image: "/images/filters/design_Styles/expressionism/Expressionism.png",
+    isSelected: false,
+  },
+  {
+    id: "Cubo-Expressionism_8_3_2",
+    name: "Cubo-Expressionism",
+    description: "Cubist elements combined with expressionism",
+    style: "Cubo-Expressionism",
+    image: "/images/filters/design_Styles/expressionism/Cubo-Expressionism.png",
+    isSelected: false,
+  },
+  {
+    id: "Figurative Expressionism_8_9_1",
+    name: "Figurative Expressionism",
+    description: "Representation of figures with expressive techniques",
+    style: "Figurative Expressionism",
+    image:
+      "/images/filters/design_Styles/expressionism/Figurative_Expressionism.png",
+    isSelected: false,
+  },
+  {
+    id: "Abstract Expressionism_8_9_2",
+    name: "Abstract Expressionism",
+    description: "Non-representational and spontaneous style",
+    style: "Abstract Expressionism",
+    image:
+      "/images/filters/design_Styles/expressionism/Abstract_Expressionism.png",
+    isSelected: false,
+  },
+];
---
+++ app/(dashboard)/create/data/filter/designStyles/neo.ts
@@ -0,0 +1,116 @@
+import type { Filter } from "../typeFilters";
+
+export const neoFilters: Filter[] = [
+  {
+    id: "Neo_9_3_1",
+    name: "Neo",
+    description: "Contemporary and innovative design style",
+    style: "Neo",
+    image: "/images/filters/design_Styles/neo/Neo.png",
+    isSelected: false,
+  },
+  {
+    id: "Neo-Baroque_9_9_1",
+    name: "Neo-Baroque",
+    description: "Revival of the ornate Baroque style with modern influences",
+    style: "Neo-Baroque",
+    image: "/images/filters/design_Styles/neo/Neo-Baroque.png",
+    isSelected: false,
+  },
+  {
+    id: "Neo-Byzantine_9_9_2",
+    name: "Neo-Byzantine",
+    description:
+      "Incorporates Byzantine art and architectural elements in a contemporary context",
+    style: "Neo-Byzantine",
+    image: "/images/filters/design_Styles/neo/Neo-Byzantine.png",
+    isSelected: false,
+  },
+  {
+    id: "Neo-Rococo_9_9_3",
+    name: "Neo-Rococo",
+    description: "Modern interpretation of the opulent Rococo style",
+    style: "Neo-Rococo",
+    image: "/images/filters/design_Styles/neo/Neo-Rococo.png",
+    isSelected: false,
+  },
+  {
+    id: "Neoclassicism_9_15_1",
+    name: "Neoclassicism",
+    description:
+      "Inspired by the classical art and architecture of ancient Greece and Rome",
+    style: "Neoclassicism",
+    image: "/images/filters/design_Styles/neo/Neoclassicism.png",
+    isSelected: false,
+  },
+  {
+    id: "Neoplasticism_9_15_2",
+    name: "Neoplasticism",
+    description:
+      "Abstract style characterized by geometric forms and primary colors",
+    style: "Neoplasticism",
+    image: "/images/filters/design_Styles/neo/Neoplasticism.png",
+    isSelected: false,
+  },
+  {
+    id: "Neo-Dada_9_21_1",
+    name: "Neo-Dada",
+    description: "Revival of the Dada movement with contemporary influences",
+    style: "Neo-Dada",
+    image: "/images/filters/design_Styles/neo/Neo-Dada.png",
+    isSelected: false,
+  },
+  {
+    id: "Neo-Futurism_9_21_2",
+    name: "Neo-Futurism",
+    description:
+      "Incorporates futuristic elements and concepts in art and design",
+    style: "Neo-Futurism",
+    image: "/images/filters/design_Styles/neo/Neo-Futurism.png",
+    isSelected: false,
+  },
+  {
+    id: "NeoSon_9_21_3",
+    name: "NeoSon",
+    description:
+      "Modern interpretation of traditional art and music of Son culture",
+    style: "NeoSon",
+    image: "/images/filters/design_Styles/neo/NeoSon.png",
+    isSelected: false,
+  },
+  {
+    id: "Neo-Tokyo_9_27_1",
+    name: "Neo-Tokyo",
+    description:
+      "Futuristic and cyberpunk-inspired design with Japanese influences",
+    style: "Neo-Tokyo",
+    image: "/images/filters/design_Styles/neo/Neo-Tokyo.png",
+    isSelected: false,
+  },
+  {
+    id: "Neo-Concretism_9_27_2",
+    name: "Neo-Concretism",
+    description:
+      "Abstract art style emphasizing geometric forms and spatial relationships",
+    style: "Neo-Concretism",
+    image: "/images/filters/design_Styles/neo/Neo-Concretism.png",
+    isSelected: false,
+  },
+  {
+    id: "Neo-Impressionism_9_33_1",
+    name: "Neo-Impressionism",
+    description:
+      "Contemporary interpretation of the Impressionist painting technique",
+    style: "Neo-Impressionism",
+    image: "/images/filters/design_Styles/neo/Neo-Impressionism.png",
+    isSelected: false,
+  },
+  {
+    id: "Neo-Expressionism_9_33_2",
+    name: "Neo-Expressionism",
+    description: "Revival of the expressive and emotive style of Expressionism",
+    style: "Neo-Expressionism",
+    image: "/images/filters/design_Styles/neo/Neo-Expressionism.png",
+    isSelected: false,
+  },
+];
