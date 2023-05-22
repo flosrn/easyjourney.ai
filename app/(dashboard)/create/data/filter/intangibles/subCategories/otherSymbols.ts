4656e0739ea99980c4e757b1888dc20a829a51ca
Flosrn <flosrn.dev@gmail.com>
2023-05-22 13:56:17 +0200

    feat(add-filters): otherSymbolsFilters

--- app/(dashboard)/create/data/filter/intangibles/subCategories/otherSymbols.ts
+++ app/(dashboard)/create/data/filter/intangibles/subCategories/otherSymbols.ts
@@ -4,7 +4,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Symbol_11_3_1",
     name: "Symbol",
-    description: "",
+    description:
+      "Infuses your images with a classic, universally recognized touch.",
     style: "Symbol",
     image: "/images/filters/Symbol.webp",
     isSelected: false,
@@ -12,7 +13,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Symbols_11_3_2",
     name: "Symbols",
-    description: "",
+    description:
+      "Embeds your visuals with multiple symbols, creating depth and intrigue.",
     style: "Symbols",
     image: "/images/filters/Symbols.webp",
     isSelected: false,
@@ -20,7 +22,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Emblem_11_9_1",
     name: "Emblem",
-    description: "",
+    description:
+      "Lends a stately, official air to your images with emblematic features.",
     style: "Emblem",
     image: "/images/filters/Emblem.webp",
     isSelected: false,
@@ -28,7 +31,7 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Sigil_11_9_2",
     name: "Sigil",
-    description: "",
+    description: "Imbues your visuals with a sense of magic and ancient power.",
     style: "Sigil",
     image: "/images/filters/Sigil.webp",
     isSelected: false,
@@ -36,7 +39,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Blissymbol_11_9_3",
     name: "Blissymbol",
-    description: "",
+    description:
+      "Translates your visuals into a modern, semantic symbolic language.",
     style: "Blissymbol",
     image: "/images/filters/Blissymbol.webp",
     isSelected: false,
@@ -44,7 +48,7 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Rune_11_15_1",
     name: "Rune",
-    description: "",
+    description: "Incorporates ancient, mystic symbols into your visuals.",
     style: "Rune",
     image: "/images/filters/Rune.webp",
     isSelected: false,
@@ -52,7 +56,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Emoticon_11_21_1",
     name: "Emoticon",
-    description: "",
+    description:
+      "Brings fun and expressiveness to your images through popular digital expressions.",
     style: "Emoticon",
     image: "/images/filters/Emoticon.webp",
     isSelected: false,
@@ -60,7 +65,7 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Emote_11_21_2",
     name: "Emote",
-    description: "",
+    description: "Infuses your visuals with a lively, emotive flair.",
     style: "Emote",
     image: "/images/filters/Emote.webp",
     isSelected: false,
@@ -68,7 +73,7 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Zodiac_11_27_1",
     name: "Zodiac",
-    description: "",
+    description: "Brings a cosmic, astrological touch to your images.",
     style: "Zodiac",
     image: "/images/filters/Zodiac.webp",
     isSelected: false,
@@ -76,7 +81,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Zodiac Sign_11_27_2",
     name: "Zodiac Sign",
-    description: "",
+    description:
+      "Adds a celestial, zodiac-sign-inspired layer to your visuals.",
     style: "Zodiac Sign",
     image: "/images/filters/Zodiac_Sign.webp",
     isSelected: false,
@@ -84,7 +90,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Alchemical-Symbols_11_33_1",
     name: "Alchemical-Symbols",
-    description: "",
+    description:
+      "Sprinkles your images with symbols hailing from the mysterious world of alchemy.",
     style: "Alchemical-Symbols",
     image: "/images/filters/Alchemical-Symbols.webp",
     isSelected: false,
@@ -92,7 +99,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Astronomical-Symbols_11_33_2",
     name: "Astronomical-Symbols",
-    description: "",
+    description:
+      "Adorns your images with a blend of celestial and astronomical symbols.",
     style: "Astronomical-Symbols",
     image: "/images/filters/Astronomical-Symbols.webp",
     isSelected: false,
@@ -100,7 +108,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Logogram_11_39_1",
     name: "Logogram",
-    description: "",
+    description:
+      "Integrates logograms, graphical symbols representing words, into your visuals.",
     style: "Logogram",
     image: "/images/filters/Logogram.webp",
     isSelected: false,
@@ -108,7 +117,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Ideogram_11_39_2",
     name: "Ideogram",
-    description: "",
+    description:
+      "Adds complexity to your visuals with ideograms, symbols representing ideas or concepts.",
     style: "Ideogram",
     image: "/images/filters/Ideogram.webp",
     isSelected: false,
@@ -116,7 +126,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Lexigram_11_45_1",
     name: "Lexigram",
-    description: "",
+    description:
+      "Blends your images with lexigrams, symbols that represent words in a graphical way.",
     style: "Lexigram",
     image: "/images/filters/Lexigram.webp",
     isSelected: false,
@@ -124,7 +135,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Lexigram Symbol_11_45_2",
     name: "Lexigram Symbol",
-    description: "",
+    description:
+      "Infuses your images with unique lexigram symbols, creating a visually captivating narrative.",
     style: "Lexigram Symbol",
     image: "/images/filters/Lexigram_Symbol.webp",
     isSelected: false,
@@ -132,7 +144,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Therblig_11_51_1",
     name: "Therblig",
-    description: "",
+    description:
+      "Incorporates therblig symbols, representing fundamental motions, into your visuals.",
     style: "Therblig",
     image: "/images/filters/Therblig.webp",
     isSelected: false,
@@ -140,7 +153,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Therblig Symbol_11_51_2",
     name: "Therblig Symbol",
-    description: "",
+    description:
+      "Enhances your images with distinctive therblig symbols, signifying elemental actions.",
     style: "Therblig Symbol",
     image: "/images/filters/Therblig_Symbol.webp",
     isSelected: false,
@@ -148,7 +162,7 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Glyph_11_57_1",
     name: "Glyph",
-    description: "",
+    description: "Imbues your visuals with glyphs, a graphic design element.",
     style: "Glyph",
     image: "/images/filters/Glyph.webp",
     isSelected: false,
@@ -156,7 +170,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Glyphigram_11_57_2",
     name: "Glyphigram",
-    description: "",
+    description:
+      "Sprinkles your images with glyphigrams, providing an interesting and artistic look.",
     style: "Glyphigram",
     image: "/images/filters/Glyphigram.webp",
     isSelected: false,
@@ -164,7 +179,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Hieroglyphica_11_63_1",
     name: "Hieroglyphica",
-    description: "",
+    description:
+      "Transforms your visuals with the enigmatic allure of ancient Egyptian hieroglyphs.",
     style: "Hieroglyphica",
     image: "/images/filters/Hieroglyphica.webp",
     isSelected: false,
@@ -172,7 +188,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Hieroglyphical_11_63_2",
     name: "Hieroglyphical",
-    description: "",
+    description:
+      "Unleashes the enchantment of the past by adding hieroglyphics to your images.",
     style: "Hieroglyphical",
     image: "/images/filters/Hieroglyphical.webp",
     isSelected: false,
@@ -180,7 +197,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Yin Yang_11_69_1",
     name: "Yin Yang",
-    description: "",
+    description:
+      "Enhances your images with the universally recognized symbol of balance and harmony.",
     style: "Yin Yang",
     image: "/images/filters/Yin_Yang.webp",
     isSelected: false,
@@ -188,7 +206,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Om Symbol_11_69_2",
     name: "Om Symbol",
-    description: "",
+    description:
+      "Infuses your images with the sacred OM symbol, lending an air of spirituality.",
     style: "Om Symbol",
     image: "/images/filters/Om_Symbol.webp",
     isSelected: false,
@@ -196,7 +215,7 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Clef_11_69_3",
     name: "Clef",
-    description: "",
+    description: "Turns up the melody by adding musical clefs to your images.",
     style: "Clef",
     image: "/images/filters/Clef.webp",
     isSelected: false,
@@ -204,7 +223,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Ouroboros_11_75_1",
     name: "Ouroboros",
-    description: "",
+    description:
+      "Enlivens your images with the Ouroboros, a potent symbol of cyclicality and recreation.",
     style: "Ouroboros",
     image: "/images/filters/Ouroboros.webp",
     isSelected: false,
@@ -212,7 +232,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Valknut_11_75_2",
     name: "Valknut",
-    description: "",
+    description:
+      "Bestows upon your visuals the Valknut, a symbol associated with the Norse god Odin.",
     style: "Valknut",
     image: "/images/filters/Valknut.webp",
     isSelected: false,
@@ -220,7 +241,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Skull and Crossbones_11_81_1",
     name: "Skull and Crossbones",
-    description: "",
+    description:
+      "Adds a hint of danger to your visuals with the classic skull and crossbones symbol.",
     style: "Skull and Crossbones",
     image: "/images/filters/Skull_and_Crossbones.webp",
     isSelected: false,
@@ -228,7 +250,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Skull and Crossbones Symbol_11_81_2",
     name: "Skull and Crossbones Symbol",
-    description: "",
+    description:
+      "Infuses your images with a pirate's spirit through the skull and crossbones symbol.",
     style: "Skull and Crossbones Symbol",
     image: "/images/filters/Skull_and_Crossbones_Symbol.webp",
     isSelected: false,
@@ -236,7 +259,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Atomic Whirl_11_87_1",
     name: "Atomic Whirl",
-    description: "",
+    description:
+      "Gives your visuals a futuristic touch with the iconic Atomic Whirl symbol.",
     style: "Atomic Whirl",
     image: "/images/filters/Atomic_Whirl.webp",
     isSelected: false,
@@ -244,7 +268,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Symbol of Chaos_11_87_2",
     name: "Symbol of Chaos",
-    description: "",
+    description:
+      "Imbues your images with a sense of unpredictability and randomness with the iconic Symbol of Chaos.",
     style: "Symbol of Chaos",
     image: "/images/filters/Symbol_of_Chaos.webp",
     isSelected: false,
@@ -252,7 +277,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Ichthys Symbol_11_87_3",
     name: "Ichthys Symbol",
-    description: "",
+    description:
+      "Integrates your images with the Ichthys symbol, infusing them with spiritual and religious undertones.",
     style: "Ichthys Symbol",
     image: "/images/filters/Ichthys_Symbol.webp",
     isSelected: false,
@@ -260,7 +286,8 @@ export const otherSymbolsFilters: Filter[] = [
   {
     id: "Croatian Interlace_11_93_1",
     name: "Croatian Interlace",
-    description: "",
+    description:
+      "Brings an intricate and traditional design element to your visuals with the Croatian Interlace pattern.",
     style: "Croatian Interlace",
     image: "/images/filters/Croatian_Interlace.webp",
     isSelected: false,
