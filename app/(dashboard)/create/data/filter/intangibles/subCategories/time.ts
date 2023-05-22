bf647d5733d78949082fe76d4fb39d12dd5663e0
Flosrn <flosrn.dev@gmail.com>
2023-05-22 14:14:13 +0200

    feat(add-filters): timeFilters

--- app/(dashboard)/create/data/filter/intangibles/subCategories/time.ts
+++ app/(dashboard)/create/data/filter/intangibles/subCategories/time.ts
@@ -4,7 +4,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Early_17_3_1",
     name: "Early",
-    description: "",
+    description:
+      "Creates a vintage and nostalgic atmosphere with the Early filter.",
     style: "Early",
     image: "/images/filters/Early.webp",
     isSelected: false,
@@ -12,7 +13,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Late_17_3_2",
     name: "Late",
-    description: "",
+    description:
+      "Adds a contemporary and modern touch to your images with the Late filter.",
     style: "Late",
     image: "/images/filters/Late.webp",
     isSelected: false,
@@ -20,7 +22,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Past_17_9_1",
     name: "Past",
-    description: "",
+    description:
+      "Transports your visuals to the past with the nostalgic Past filter.",
     style: "Past",
     image: "/images/filters/Past.webp",
     isSelected: false,
@@ -28,7 +31,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Future_17_9_2",
     name: "Future",
-    description: "",
+    description:
+      "Creates a futuristic and sci-fi-inspired look with the Future filter.",
     style: "Future",
     image: "/images/filters/Future.webp",
     isSelected: false,
@@ -36,7 +40,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Time_17_15_1",
     name: "Time",
-    description: "",
+    description:
+      "Applies a time-related effect to your images with the Time filter.",
     style: "Time",
     image: "/images/filters/Time.webp",
     isSelected: false,
@@ -44,7 +49,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Present-Time_17_15_2",
     name: "Present-Time",
-    description: "",
+    description:
+      "Represents the current moment and time with the Present-Time filter.",
     style: "Present-Time",
     image: "/images/filters/Present-Time.webp",
     isSelected: false,
@@ -52,7 +58,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Current-Time_17_15_3",
     name: "Current-Time",
-    description: "",
+    description:
+      "Captures the essence of the current time and moment in your visuals with the Current-Time filter.",
     style: "Current-Time",
     image: "/images/filters/Current-Time.webp",
     isSelected: false,
@@ -60,7 +67,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Second_17_21_1",
     name: "Second",
-    description: "",
+    description:
+      "Adds a time-related effect that represents seconds to your images with the Second filter.",
     style: "Second",
     image: "/images/filters/Second.webp",
     isSelected: false,
@@ -68,7 +76,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Minute_17_21_2",
     name: "Minute",
-    description: "",
+    description:
+      "Applies a time-related effect that represents minutes to your visuals with the Minute filter.",
     style: "Minute",
     image: "/images/filters/Minute.webp",
     isSelected: false,
@@ -76,7 +85,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Hour_17_21_3",
     name: "Hour",
-    description: "",
+    description:
+      "Represents the passage of hours with the Hour filter, adding a time-related effect to your images.",
     style: "Hour",
     image: "/images/filters/Hour.webp",
     isSelected: false,
@@ -84,7 +94,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Week_17_27_1",
     name: "Week",
-    description: "",
+    description:
+      "Applies a time-related effect that represents weeks to your visuals with the Week filter.",
     style: "Week",
     image: "/images/filters/Week.webp",
     isSelected: false,
@@ -92,7 +103,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Month_17_27_2",
     name: "Month",
-    description: "",
+    description:
+      "Captures the essence of months and their passage with the Month filter.",
     style: "Month",
     image: "/images/filters/Month.webp",
     isSelected: false,
@@ -100,7 +112,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Year_17_27_3",
     name: "Year",
-    description: "",
+    description:
+      "Adds a time-related effect that represents years to your images with the Year filter.",
     style: "Year",
     image: "/images/filters/Year.webp",
     isSelected: false,
@@ -108,7 +121,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Decade_17_33_1",
     name: "Decade",
-    description: "",
+    description:
+      "Transports your visuals to a specific decade with the Decade filter.",
     style: "Decade",
     image: "/images/filters/Decade.webp",
     isSelected: false,
@@ -116,7 +130,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Millennia_17_33_2",
     name: "Millennia",
-    description: "",
+    description:
+      "Represents thousands of years and adds a time-related effect to your images with the Millennia filter.",
     style: "Millennia",
     image: "/images/filters/Millennia.webp",
     isSelected: false,
@@ -124,7 +139,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Epoch_17_39_1",
     name: "Epoch",
-    description: "",
+    description:
+      "Captures the essence of a significant period in history with the Epoch filter.",
     style: "Epoch",
     image: "/images/filters/Epoch.webp",
     isSelected: false,
@@ -132,7 +148,8 @@ export const timeFilters: Filter[] = [
   {
     id: "Aeon_17_39_2",
     name: "Aeon",
-    description: "",
+    description:
+      "Represents an extended period of time and adds a time-related effect to your visuals with the Aeon filter.",
     style: "Aeon",
     image: "/images/filters/Aeon.webp",
     isSelected: false,
