f226fbeed36ae212462ae5070c62e17eb1e3ab1d
MDegano <123469601+MDeganoWDev@users.noreply.github.com>
2023-05-24 09:58:08 +0200

    migrate api/posters/like

---
+++ app/api/posters/like/route.ts
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import { getServerAuthSession } from "~/server/auth";
+import { prisma } from "~/server/db/prisma";
+
+export async function POST(request: Request) {
+  const session = await getServerAuthSession();
+
+  if (!session) {
+    return NextResponse.json({ status: 401, message: "Unauthorized" });
+  }
+
+  const body = await request.json();
+  const { posterId } = body;
+
+  if (!posterId) {
+    return NextResponse.json({ status: 400, message: "Poster ID is required" });
+  }
+
+  try {
+    // Check if the like already exists
+    const existingLike = await prisma.like.findFirst({
+      where: {
+        userId: session.user.id,
+        posterId,
+      },
+    });
+
+    if (existingLike) {
+      // Delete a like
+      await prisma.like.delete({
+        where: {
+          id: existingLike.id,
+        },
+      });
+      return NextResponse.json({ status: 200 });
+    } else {
+      // Create a new like
+      await prisma.like.create({
+        data: {
+          userId: session.user.id,
+          posterId,
+        },
+      });
+      return NextResponse.json({ status: 201 });
+    }
+  } catch {
+    return NextResponse.json({
+      status: 500,
+      message: "Failed to like the poster",
+    });
+  }
+}
