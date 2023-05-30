41de1c591125dbc162dbacfe7e1a03d95a2d3b1e
Flosrn <flosrn.dev@gmail.com>
2023-05-30 09:08:54 +0200

    feat(poster): improve anim

--- src/components/posters/BackToPreviousPageButton.tsx
+++ src/components/posters/BackToPreviousPageButton.tsx
@@ -12,11 +12,11 @@ const BackToPreviousPageButton = ({}: BackToPreviousPageButtonProps) => {
     <div className="mb-5">
       <button
         onClick={() => router.back()}
-        className="group relative flex items-center"
+        className="group relative flex cursor-pointer items-center"
       >
         <ChevronLeftIcon className="absolute mr-2 h-5 w-5 opacity-100 transition-opacity duration-300 ease-in-out group-hover:opacity-0" />
         <ArrowLeftIcon className="absolute left-[4px] mr-2 h-5 w-5 opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100" />
-        <span className="ml-6 transition-all duration-300 ease-in-out group-hover:ml-8">
+        <span className="ml-6 cursor-pointer transition-all duration-300 ease-in-out group-hover:ml-8">
           Back
         </span>
       </button>
