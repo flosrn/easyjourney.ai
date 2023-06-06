fa3afb3b00cc0c4b3de6ed404b761f3ef519d296
Flosrn <flosrn.dev@gmail.com>
2023-06-06 18:29:28 +0200

    feat(poster): add zoom image

--- src/components/posters/poster-image-container.tsx
+++ src/components/posters/poster-image-container.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Image from "next/image";
 
-import ExpandedPosterDialog from "~/components/posters/expanded-poster-dialog";
 import LikeButton from "~/components/posters/like-button";
+import ZoomImage from "~/components/posters/zoom-image";
 
 import { cn } from "~/lib/classNames";
 import type { PosterType } from "~/types/poster";
@@ -22,20 +22,14 @@ const PosterImageContainer = ({
       "sm:max-w-[80%]": ratio === "4/7",
     })}
   >
-    <ExpandedPosterDialog
-      posterImage={image}
-      width={width}
-      height={height}
-      prompt={prompt}
-    >
-      <Image
-        src={image}
-        alt={prompt}
-        width={width ?? 500}
-        height={height ?? 500}
-        className="h-auto max-h-[85vh] w-full rounded-md object-cover"
-      />
-    </ExpandedPosterDialog>
+    <Image
+      src={image}
+      alt={prompt}
+      width={width ?? 500}
+      height={height ?? 500}
+      className="h-auto max-h-[85vh] w-full rounded-md object-cover"
+    />
+    <ZoomImage imageUrl={image} alt={prompt} />
     <div className="mt-2 inline-block max-w-max rounded-3xl border bg-gray-700/20 px-2 py-1 hover:bg-gray-700/80">
       <LikeButton id={id} likes={likes} />
     </div>
