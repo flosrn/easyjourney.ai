6f8cb68393aeb78bc7b7b5e970e2b3ff197ce22f
HugoSbl <hugo.sablot@gmail.com>
2023-06-14 22:08:06 +0200

    feat(my-feed): my-feed page and call

--- app/(marketing)/posters/(categories)/my-feed/page.tsx
+++ app/(marketing)/posters/(categories)/my-feed/page.tsx
@@ -1,22 +1,40 @@
 import React, { Suspense } from "react";
+import { getServerAuthSession } from "~/server/auth";
 import { prisma } from "~/server/db/prisma";
 
 import Posters from "../../components/posters";
 
-const getPostersOfFollowedUsers = async () => {
-  return prisma.poster.findMany({
-    orderBy: {
-      createdAt: "desc",
+const getNewPosters = async ({ userId }: string) => {
+  const followedUsers = await prisma.user.findMany({
+    where: {
+      followers: {
+        some: {
+          followerId: userId,
+        },
+      },
     },
-    include: { user: true, likes: true },
+    include: {
+      posters: {
+        orderBy: {
+          createdAt: "desc",
+        },
+        include: { user: true, likes: true },
+      },
+    },
   });
+  return followedUsers.flatMap((user) => user.posters);
 };
 
 export default async function MyFeed() {
-  const posters = await getPostersOfFollowedUsers();
+  const session = await getServerAuthSession();
+  console.log("session :", session);
+
+  const posters = await getNewPosters(session?.user.id);
+  console.log("posters :", posters);
   return (
     <>
       <section className="container mt-6 grid items-center justify-center gap-6 pb-8">
+        <h1 className="text-3xl font-bold">Nouveaux posters</h1>
         <Suspense fallback={<div>Loading posters...</div>}>
           <Posters posters={posters} />
         </Suspense>
