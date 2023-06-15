77afcc3fc04614b6739e000a5ed0ad341b0e7539
Flosrn <flosrn.dev@gmail.com>
2023-06-15 16:59:42 +0200

    fix(subscription): fix ts errors

---
+++ app/(dashboard)/settings/components/sidebar-nav.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { buttonVariants } from "~/components/ui/button";
+
+import { cn } from "~/lib/classNames";
+
+type SidebarNavProps = React.HTMLAttributes<HTMLElement> & {
+  items: {
+    href: string;
+    title: string;
+  }[];
+};
+
+const SidebarNav = ({ className, items, ...props }: SidebarNavProps) => {
+  const pathname = usePathname();
+
+  return (
+    <nav
+      className={cn(
+        "flex space-x-2 lg:flex-col lg:space-x-0 lg:space-y-1",
+        className
+      )}
+      {...props}
+    >
+      {items.map((item) => (
+        <Link
+          key={item.href}
+          href={item.href}
+          className={cn(
+            buttonVariants({ variant: "ghost" }),
+            pathname === item.href
+              ? "bg-muted hover:bg-muted"
+              : "hover:bg-transparent hover:underline",
+            "justify-start"
+          )}
+        >
+          {item.title}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default SidebarNav;
