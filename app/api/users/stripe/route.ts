f9d5698848e648848bac46f8337796161a5c6b10
Flosrn <flosrn.dev@gmail.com>
2023-06-17 13:04:36 +0200

    fix(subscription): add logs

--- app/api/users/stripe/route.ts
+++ app/api/users/stripe/route.ts
@@ -1,72 +1,65 @@
 import { NextResponse } from "next/server";
+import { getServerAuthSession } from "~/server/auth";
+import { z } from "zod";
 
-// import { getServerAuthSession } from "~/server/auth";
-// import { z } from "zod";
-//
-// import { stripe } from "~/lib/stripe";
-// import { getUserSubscriptionPlan } from "~/lib/subscriptions";
-// import { proPlan } from "~/config/subscriptions";
+import { stripe } from "~/lib/stripe";
+import { getUserSubscriptionPlan } from "~/lib/subscriptions";
+import { proPlan } from "~/config/subscriptions";
 
 console.log("/api/users/stripe/route.ts");
 
 export async function GET(request: Request) {
-  const subscriptionUrl = `https://myposter-preview.vercel.app/settings/subscription`;
-  console.log("subscriptionUrl :", subscriptionUrl);
+  try {
+    const subscriptionUrl =
+      "https://myposter-preview.vercel.app/settings/subscription";
+    console.log("subscriptionUrl :", subscriptionUrl);
+    const session = await getServerAuthSession();
+    console.log("session :", session);
 
-  return NextResponse.json({ url: subscriptionUrl });
+    if (!session?.user || !session.user.email) {
+      return NextResponse.json(null, { status: 403 });
+    }
 
-  // try {
-  //   const session = await getServerAuthSession();
-  //   console.log("session :", session);
-  //
-  //   if (!session?.user || !session.user.email) {
-  //     return NextResponse.json(null, { status: 403 });
-  //   }
-  //
-  //   const subscriptionPlan = await getUserSubscriptionPlan(session.user.id);
-  //   console.log("subscriptionPlan :", subscriptionPlan);
-  //
-  //   console.log("env.NEXT_PUBLIC_URL :", env.NEXT_PUBLIC_URL);
-  //   const subscriptionUrl = `${env.NEXT_PUBLIC_URL}/settings/subscription`;
-  //
-  //   // The user is on the pro plan.
-  //   // Create a portal session to manage subscription.
-  //   if (subscriptionPlan.isPro && subscriptionPlan.stripeCustomerId) {
-  //     const stripeSession = await stripe.billingPortal.sessions.create({
-  //       customer: subscriptionPlan.stripeCustomerId,
-  //       return_url: subscriptionUrl,
-  //     });
-  //
-  //     return NextResponse.json({ url: stripeSession.url });
-  //   }
-  //
-  //   // The user is on the free plan.
-  //   // Create a checkout session to upgrade.
-  //   const stripeSession = await stripe.checkout.sessions.create({
-  //     success_url: subscriptionUrl,
-  //     cancel_url: subscriptionUrl,
-  //     payment_method_types: ["card"],
-  //     mode: "subscription",
-  //     billing_address_collection: "auto",
-  //     customer_email: session.user.email,
-  //     line_items: [
-  //       {
-  //         price: proPlan.stripePriceId,
-  //         quantity: 1,
-  //       },
-  //     ],
-  //     metadata: {
-  //       userId: session.user.id,
-  //     },
-  //   });
-  //
-  //   return NextResponse.json({ url: stripeSession.url });
-  // } catch (error: unknown) {
-  //   console.log("error :", error);
-  //   if (error instanceof z.ZodError) {
-  //     return NextResponse.json({ status: 422, error: error.issues });
-  //   }
-  //
-  //   return NextResponse.json({ status: 500, error });
-  // }
+    const subscriptionPlan = await getUserSubscriptionPlan(session.user.id);
+
+    // The user is on the pro plan.
+    // Create a portal session to manage subscription.
+    if (subscriptionPlan.isPro && subscriptionPlan.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: subscriptionPlan.stripeCustomerId,
+        return_url: subscriptionUrl,
+      });
+
+      return NextResponse.json({ url: stripeSession.url });
+    }
+
+    // The user is on the free plan.
+    // Create a checkout session to upgrade.
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: subscriptionUrl,
+      cancel_url: subscriptionUrl,
+      payment_method_types: ["card"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      customer_email: session.user.email,
+      line_items: [
+        {
+          price: proPlan.stripePriceId,
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId: session.user.id,
+      },
+    });
+
+    return NextResponse.json({ url: stripeSession.url });
+  } catch (error: unknown) {
+    console.log("error :", error);
+    if (error instanceof z.ZodError) {
+      return NextResponse.json({ status: 422, error: error.issues });
+    }
+
+    return NextResponse.json({ status: 500, error });
+  }
 }
--- src/utils/absoluteUrl.ts
+++ src/utils/absoluteUrl.ts
@@ -1,8 +1,8 @@
 import { env } from "~/env.mjs";
 
 const absoluteUrl = (path: string) => {
-  console.log("env.NEXT_PUBLIC_URL :", env.NEXT_PUBLIC_URL);
-  return `${env.NEXT_PUBLIC_URL}${path}`;
+  console.log("env.NEXT_PUBLIC_URL :", env.NEXTAUTH_URL);
+  return `${env.NEXTAUTH_URL}${path}`;
 };
 
 export default absoluteUrl;
