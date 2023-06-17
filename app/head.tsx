73631cfbb858fa055accbedc6cf62ceb2631d3f3
Flosrn <flosrn.dev@gmail.com>
2023-06-17 20:05:41 +0200

    feat(subscription): add missing comma

--- app/head.tsx
+++ app/head.tsx
@@ -10,7 +10,7 @@ export default function Head() {
       />
       <meta
         name="viewport"
-        content="width=device-width, initial-scale=1 maximum-scale=1, user-scalable=0"
+        content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
       />
 
       <link rel="icon" href="/favicon.ico" />
