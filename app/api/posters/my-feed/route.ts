58a6af701499269736ff9a9343c1974795eac2ed
Flosrn <flosrn.dev@gmail.com>
2023-06-18 15:12:22 +0200

    feat(subscription): improve types

--- app/api/posters/my-feed/route.ts
+++ app/api/posters/my-feed/route.ts
@@ -1,16 +1,13 @@
 import { NextResponse } from "next/server";
+import type { Poster } from "@prisma/client";
 import { getServerAuthSession } from "~/server/auth";
 import { prisma } from "~/server/db/prisma";
 
-type getFollowersprops = {
-  userId: string | undefined;
-};
-
-type getPostersProps = {
-  followersArray: string[];
-};
-
-const getFollowers = async ({ userId }: getFollowersprops) => {
+const getFollowers = async ({
+  userId,
+}: {
+  userId?: string;
+}): Promise<{ followingId: string }[]> => {
   const followedUserIds = await prisma.follow.findMany({
     where: {
       followerId: userId,
@@ -23,7 +20,11 @@ const getFollowers = async ({ userId }: getFollowersprops) => {
   return followedUserIds;
 };
 
-const getPosters = async ({ followersArray }: getPostersProps) => {
+const getPosters = async ({
+  followersArray,
+}: {
+  followersArray: string[];
+}): Promise<Poster[]> => {
   const posters = await prisma.poster.findMany({
     where: {
       isPublic: true,
@@ -47,11 +48,11 @@ export async function GET(request: Request) {
   try {
     const followers = await getFollowers({ userId });
     const followersArray = followers.map(({ followingId }) => followingId);
-    console.log("followersArray :", followersArray);
     const posters = await getPosters({ followersArray });
 
     return NextResponse.json({ status: 200, posters });
   } catch (error: unknown) {
+    console.error("error", error);
     return NextResponse.json({
       status: 500,
       message: `Internal Server Error : ${error}`,
