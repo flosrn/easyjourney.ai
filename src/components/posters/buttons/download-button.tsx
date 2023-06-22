e267fd1f598449ef12dea43175dd3da3e1046eee
Flosrn <flosrn.dev@gmail.com>
2023-06-22 21:58:17 +0200

    improve download and share button

--- src/components/posters/buttons/download-button.tsx
+++ src/components/posters/buttons/download-button.tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/navigation";
 import { saveAs } from "file-saver";
 import { DownloadIcon } from "lucide-react";
 import { useSession } from "next-auth/react";
+import toast from "react-hot-toast";
 
 import TooltipButton from "~/components/posters/buttons/tooltip-button";
 
@@ -18,17 +19,39 @@ const DownloadButton = ({ imageUrl, filename }: DownloadButtonProps) => {
   const { data: session } = useSession();
   const router = useRouter();
 
+  const fakeDownload = async () => {
+    if (!imageUrl) return;
+    if (!session) return router.push("/api/auth/signin");
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        try {
+          const hasWebp = imageUrl.includes(".webp");
+          const imageUrlWithoutExtension =
+            hasWebp && imageUrl.split(".webp")[0];
+          const filenameWithoutExtension =
+            hasWebp && filename?.split(".webp")[0];
+          const imageInPng = hasWebp
+            ? `${imageUrlWithoutExtension}.png`
+            : imageUrl;
+          const filenameInPng = hasWebp
+            ? `${filenameWithoutExtension}.png`
+            : filename;
+          saveAs(imageInPng, filenameInPng ?? "poster.png");
+          resolve(true);
+        } catch {
+          resolve(false);
+        }
+      }, 2000);
+    });
+  };
+
   const handleDownload = async (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    if (!imageUrl) return;
-    if (!session) {
-      return router.push("/api/auth/signin");
-    }
-    const imageUrlWithoutExtension = imageUrl.split(".webp")[0];
-    const filenameWithoutExtension = filename?.split(".webp")[0];
-    const imageInPng = `${imageUrlWithoutExtension}.png`;
-    const filenameInPng = `${filenameWithoutExtension}.png`;
-    saveAs(imageInPng, filenameInPng);
+    await toast.promise(fakeDownload(), {
+      loading: "Downloading...",
+      success: "Poster downloaded!",
+      error: "Something went wrong",
+    });
   };
 
   return (
--- src/components/posters/buttons/share-poster-button.tsx
+++ src/components/posters/buttons/share-poster-button.tsx
@@ -2,14 +2,20 @@
 
 import React from "react";
 import { Share2Icon } from "lucide-react";
+import toast from "react-hot-toast";
 
 import TooltipButton from "~/components/posters/buttons/tooltip-button";
 
 type SharePosterButtonProps = {};
 
 const SharePosterButton = ({}: SharePosterButtonProps) => {
+  const handleSharePoster = () => {
+    toast("Coming soon!", {
+      icon: "🚧",
+    });
+  };
   return (
-    <TooltipButton Icon={Share2Icon} clickHandler={() => null}>
+    <TooltipButton Icon={Share2Icon} clickHandler={handleSharePoster}>
       Share poster
     </TooltipButton>
   );
