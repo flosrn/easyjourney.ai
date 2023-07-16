4814b9772778601ef8ab50a68fd5c712bd146e12
Flosrn <flosrn.dev@gmail.com>
2023-07-16 19:15:21 +0200

    add CTA

--- app/page.tsx
+++ app/page.tsx
@@ -253,6 +253,62 @@ export default async function IndexPage() {
             </div>
           </div>
         </section>
+
+        <div className="mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8">
+          <div className="relative isolate overflow-hidden bg-muted px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0">
+            <svg
+              viewBox="0 0 1024 1024"
+              className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-y-1/2 [mask-image:radial-gradient(closest-side,white,transparent)] sm:left-full sm:-ml-80 lg:left-1/2 lg:ml-0 lg:-translate-x-1/2 lg:translate-y-0"
+              aria-hidden="true"
+            >
+              <circle
+                cx={512}
+                cy={512}
+                r={512}
+                fill="url(#759c1415-0410-454c-8f7c-9a820de03641)"
+                fillOpacity="0.7"
+              />
+              <defs>
+                <radialGradient id="759c1415-0410-454c-8f7c-9a820de03641">
+                  <stop stopColor="#7775D6" />
+                  <stop offset={1} stopColor="#E935C1" />
+                </radialGradient>
+              </defs>
+            </svg>
+            <div className="mx-auto max-w-md text-center lg:mx-0 lg:flex-auto lg:py-32 lg:text-left">
+              <h2 className="text-3xl font-bold tracking-tight text-foreground sm:text-4xl">
+                Join our art community.
+                <br />
+                Connect and share today.
+              </h2>
+              <p className="mt-6 text-lg leading-8 text-muted-foreground">
+                Join our vibrant AI-art community. Connect, share, and dive into
+                the world of AI creativity.
+              </p>
+              <div className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start">
+                <Button asChild>
+                  <Link href="#">Join now</Link>
+                </Button>
+                <Link
+                  href="/about/faq"
+                  className="text-sm font-semibold leading-6 text-foreground"
+                >
+                  Learn more <span aria-hidden="true">→</span>
+                </Link>
+              </div>
+            </div>
+            <div className="relative mt-16 h-80 lg:mt-8">
+              <Image
+                className="absolute left-0 top-0 w-[50rem] max-w-none rounded-md bg-white/5 ring-1 ring-white/10"
+                src="/images/landingpage/cta/profile.png"
+                alt="Profile screenshot"
+                width={3110}
+                height={1988}
+              />
+            </div>
+          </div>
+        </div>
+
         <section
           id="features-community-section"
           className="flex-center container overflow-hidden bg-background py-24 sm:py-32"
@@ -281,7 +337,7 @@ export default async function IndexPage() {
                             aria-hidden="true"
                           />
                           {feature.name}
-                        </dt>{" "}
+                        </dt>
                         <dd className="inline">{feature.description}</dd>
                       </div>
                     ))}
