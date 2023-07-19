a0cbd2a0f6fe7d99849c70c1ea5eb72d08e934a5
Flosrn <flosrn.dev@gmail.com>
2023-07-19 17:50:03 +0200

    add og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,8 @@ const inter = Inter({
   variable: "--font-inter",
 });
 
+const image = `${siteConfig.url}/og.png`;
+
 export const metadata: Metadata = {
   title: {
     default: siteConfig.title,
@@ -50,12 +52,13 @@ export const metadata: Metadata = {
     title: siteConfig.title,
     description: siteConfig.description,
     siteName: siteConfig.title,
+    images: [image],
   },
   twitter: {
     card: "summary_large_image",
     title: siteConfig.title,
     description: siteConfig.description,
-    images: [`${siteConfig.url}/og.jpg`],
+    images: [image],
     creator: "@flosrn",
   },
   icons: {
