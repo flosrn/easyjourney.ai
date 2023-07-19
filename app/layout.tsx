f769caf91ba04d8e6795655890d234bcd23a2d1b
Flosrn <flosrn.dev@gmail.com>
2023-07-19 18:13:59 +0200

    add Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import type { ReactNode } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import { ServerThemeProvider } from "next-themes";
 
 import NavigationEvents from "~/components/events/navigation-events";
@@ -84,6 +85,7 @@ export default async function RootLayout({
       <html lang="en" className={inter.className} suppressHydrationWarning>
         <body className="h-screen bg-background font-sans text-foreground antialiased transition-all duration-200 ease-in-out">
           <Providers>{children}</Providers>
+          <Analytics />
           <Suspense fallback={null}>
             <NavigationEvents />
           </Suspense>
