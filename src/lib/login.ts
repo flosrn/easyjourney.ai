19991af27d9e3aa85175daf49ec462b4de60a844
Flosrn <flosrn.dev@gmail.com>
2023-07-20 18:25:05 +0200

    error toast

--- src/lib/login.ts
+++ src/lib/login.ts
@@ -4,8 +4,8 @@ import { toast } from "react-hot-toast";
 
 export async function login() {
   if (isWebview(window.navigator.userAgent)) {
-    toast(
-      "Open Easyjourney in Safari or Chrome to sign in.\n\nGoogle does not support embedded browser.",
+    toast.error(
+      "Please open Easyjourney in Safari or Chrome to sign in.\n\nGoogle does not support embedded browser.",
       {
         duration: 10000,
       }
