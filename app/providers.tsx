7b47b50382577a3311aebba7f85c70d8b2499c73
Flosrn <flosrn.dev@gmail.com>
2023-10-24 15:16:01 +0200

    fix error

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type { ReactNode } from "react";
-// eslint-disable-next-line import/named
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
